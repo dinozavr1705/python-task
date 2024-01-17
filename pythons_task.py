905a8ae14888b095d5c97222e9297c5b4f74ccab
dinozavr1705 <151431068+dinozavr1705@users.noreply.github.com>
2024-01-17 10:17:07 +0700

    Create pythons_task.py
    алгоритм перебора быстрее и точнее чем алгоритм стирлинга

---
+++ pythons_task.py
@@ -0,0 +1,29 @@
+import math
+from time import *
+from math import *
+n = 123
+a = time()
+def stirling_alghoritm(n):
+    a = sqrt(2 * 2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821)
+    b = pow(n/math.e,n)
+    c =1 + (1 / sqrt(52 * math.e))
+    print(a * b + c)
+stirling_alghoritm(n)
+b = time()
+c = b - a
+s = time()
+def factorial(a):
+    d = 1
+    c = 2
+    while c!=a + 1:
+        d = d * c
+        c = c + 1
+    print(d)
+factorial(123)
+l = time()
+k = l - s
+if c < k:
+    print("метод стерлинга быстрее")
+else:
+    print("метод перебора быстрее")
+
